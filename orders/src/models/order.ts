fe43d51693c252236881ba51e8b1a43108d08477
Brandon Lee <brandonlee2543@hotmail.com>
2022-08-29 21:31:00 -0400

    Begin skaffolding our reference system between mongoDB collections

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,23 +1,31 @@
 import mongoose from "mongoose";
+import { ClassStaticBlockDeclaration } from "typescript";
 
 interface OrderAttrs {
-   quantity: number;
    delivery_date: string;
    payment_made: boolean;
    payment_date: string;
    is_delivery: boolean;
    driver_paid: boolean;
    warehouse_paid: boolean;
+
+   // Conceptual linking of our Order with a Customer and Stock.
+   // Current thought is that we'll want a customer who'll be linked to this Order, as well as
+   // the actual contents of said order. That being said, final vision might be Just Stock
+   // linked here, and then Order becomes an optional parameter in our Customers service itself.
+   customer: CustomerDoc;
+   chow_being_ordered: ChowDoc;
 }
 
 interface OrderDoc extends mongoose.Document {
-   quantity: number;
    delivery_date: string;
    payment_made: boolean;
    payment_date: string;
    is_delivery: boolean;
    driver_paid: boolean;
    warehouse_paid: boolean;
+   customer: CustomerDoc;
+   chow_being_ordered: ChowDoc;
 }
 
 interface OrderModel extends mongoose.Model<OrderDoc> {
@@ -26,13 +34,22 @@ interface OrderModel extends mongoose.Model<OrderDoc> {
 
 const orderSchema = new mongoose.Schema(
    {
-      quantity: { type: Number, required: true },
       delivery_date: { type: String, required: true },
       payment_date: { type: String, required: true },
       payment_made: { type: Boolean, required: true },
       is_delivery: { type: Boolean, required: true },
       driver_paid: { type: Boolean, required: true },
       warehouse_paid: { type: Boolean, required: true },
+
+      // Setting up reference for our Customer and our stock
+      customer: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Customer",
+      },
+      chow_being_ordered: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Chow",
+      },
    },
    {
       toJSON: {
