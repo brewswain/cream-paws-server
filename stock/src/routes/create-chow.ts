ddad48109c534df9a79831dc82de36d7ef3eeb83
Brandon Lee <brandonlee2543@hotmail.com>
2022-09-01 01:43:23 -0400

    Created Chow Creation Publisher

--- stock/src/routes/create-chow.ts
+++ stock/src/routes/create-chow.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { requireAuth, validateRequest } from "@cream-paws-util/common";
 import { body } from "express-validator";
 import { Chow } from "../models/chow";
+import { ChowCreatedPublisher } from "../events/publishers/chow-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -35,6 +37,20 @@ router.post(
       });
 
       await chow.save();
+
+      new ChowCreatedPublisher(natsWrapper.client).publish({
+         id: chow.id,
+         version: chow.version,
+         brand: chow.brand,
+         target_group: chow.target_group,
+         flavour: chow.flavour,
+         size: chow.size,
+         unit: chow.unit,
+         quantity: chow.quantity,
+         wholesale_price: chow.wholesale_price,
+         retail_price: chow.retail_price,
+         is_paid_for: chow.is_paid_for,
+      });
 
       res.status(201).send(chow);
    }
